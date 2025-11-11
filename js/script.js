486d542f25c4444eb7330af9601dfc6a00bc2780
Aleksandar <89696393+Se9z11@users.noreply.github.com>
2025-11-11 12:17:44 +0100

    feat: add GSAP animations for hero section
    - Implemented heading, subtext, and CTA animation timeline
    - Added floating random motion for blobs

--- js/script.js
+++ js/script.js
@@ -1,4 +1,32 @@
-import {gsap} from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
+
+const tl = gsap.timeline();
+
+const blobs = document.querySelectorAll(".blob");
+
+blobs.forEach(blob =>{
+    gsap.to(blob,{
+        x: ()=> gsap.utils.random(-200, 200),
+        y: ()=> gsap.utils.random(-300,300),
+        duration: ()=> gsap.utils.random(2, 4),
+        ease: "sine.inOut",
+        repeat: -1,
+        yoyo: true
+    });
+});
+
+tl.from(".hero h1",{
+    y:40,
+    opacity: 0,
+    duration: 1.2,
+    ease: "power3.out"
+},0.5);
+
+tl.from(["#hero-p", ".people-wrapper", ".btn"],{
+    y:20,
+    opacity: 0,
+    duration: 1.2,
+    stagger: 0.2, 
+    ease: "power2.out"
+}, "-=0.6")
