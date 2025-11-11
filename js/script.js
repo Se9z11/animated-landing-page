7d1f455208a78da56e6d8b8be63c4b365b4daf5f
Aleksandar <89696393+Se9z11@users.noreply.github.com>
2025-11-11 22:01:04 +0100

    feat: added infinite blob movement and scroll trigger animations

--- js/script.js
+++ js/script.js
@@ -5,16 +5,17 @@ const tl = gsap.timeline();
 
 const blobs = document.querySelectorAll(".blob");
 
-blobs.forEach(blob =>{
+function moveBlobs(blob){
     gsap.to(blob,{
-        x: ()=> gsap.utils.random(-200, 200),
-        y: ()=> gsap.utils.random(-300,300),
-        duration: ()=> gsap.utils.random(2, 4),
-        ease: "sine.inOut",
-        repeat: -1,
-        yoyo: true
-    });
-});
+    x: gsap.utils.random(-200, 200),
+    y: gsap.utils.random(-200, 200),
+    duration: gsap.utils.random(3, 6),
+    ease: "sine.inOut",
+    onComplete: ()=> moveBlobs(blob)
+    })
+}
+
+blobs.forEach(blob => moveBlobs(blob))
 
 tl.from(".hero h1",{
     y:40,
@@ -30,3 +31,15 @@ tl.from(["#hero-p", ".people-wrapper", ".btn"],{
     stagger: 0.2, 
     ease: "power2.out"
 }, "-=0.6")
+
+gsap.to(".hero-content", {
+  scrollTrigger: {
+    trigger: ".hero",       
+    start: "top -30%",        
+    end: "bottom top",       
+    scrub: true,             
+  },
+  y: -100,                  
+  opacity: 0,                
+  ease: "power2.out"
+});
